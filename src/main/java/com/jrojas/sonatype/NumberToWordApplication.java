7ad3ee633a5cbff9c948666093cfc60b8fb7b605
jrojas <johanna.rojas@tessi.fr>
2019-07-22 23:51:00 +0200

    initial commit : add the main class

---
+++ src/main/java/com/jrojas/sonatype/NumberToWordApplication.java
@@ -0,0 +1,13 @@
+package com.jrojas.sonatype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberToWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberToWordApplication.class, args);
+	}
+
+}
