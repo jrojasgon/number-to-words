c47a682bb9ca091ef214e40391531409ffd966d9
jrojas <johanna.rojas@tessi.fr>
2019-07-24 00:11:19 +0200

    add a file for constants

---
+++ src/main/java/com/jrojas/sonatype/constants/NumberToWordConstants.java
@@ -0,0 +1,27 @@
+package com.jrojas.sonatype.constants;
+
+/**
+ * Class to define the constants values
+ */
+public final class NumberToWordConstants {
+
+    private NumberToWordConstants() {
+    }
+
+    public static final String EMPTY = "";
+    public static final String EMPTY_VALUE = "Empty values are not allowed";
+    public static final String RANGE_ERROR = "No floating point numbers, only integers : Value must be between -2 147 483 648 and 2 147 483 647";
+    public static final String SEPARATOR = "-";
+    public static final String NEGATIVE_PREFIX = "Negative ";
+    public static final String AND = "and ";
+
+    public static final String BILLION = " billion ";
+    public static final String MILLION = " million ";
+    public static final String THOUSAND = " thousand ";
+    public static final String HUNDRED = " hundred ";
+
+    public static final int ONE_BILLION =1_000_000_000;
+    public static final int ONE_MILLION =1_000_000;
+    public static final int ONE_THOUSAND =1_000;
+    public static final int ONE_HUNDRED =100;
+}
