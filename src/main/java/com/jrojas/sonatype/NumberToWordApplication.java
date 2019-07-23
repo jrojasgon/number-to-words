bb09c5e8f03cb2ed87e71905bc5d026120f5304b
jrojas <johanna.rojas@tessi.fr>
2019-07-24 00:08:44 +0200

    [POM] delete useless dependencies

--- src/main/java/com/jrojas/sonatype/NumberToWordApplication.java
+++ src/main/java/com/jrojas/sonatype/NumberToWordApplication.java
@@ -3,11 +3,14 @@ package com.jrojas.sonatype;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot starter class
+ */
 @SpringBootApplication
 public class NumberToWordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NumberToWordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NumberToWordApplication.class, args);
+    }
 
 }
